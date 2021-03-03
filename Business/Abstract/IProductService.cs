73df57271e50507773d8bc899c382b3f3608a1c1
Berat Taner Indere <berat.indere@outlook.com>
2021-03-03 14:35:23 +0300

    3 Mart Saat 14:35

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -10,11 +10,11 @@ namespace Business.Abstract
     //İş katmanında kullanacağımız servis operasyonları.
     public interface IProductService
     {
-        List<Product> GetAll(); //Tümünü getir.
-        List<Product> GetAllByCategoryId(int id); //by:ile categoryId göre getir.
-        List<Product> GetByUnitPrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
-        Product GetById(int productId); //Tek başına bir ürün döndürür.
-        IResult Add(Product product);  //Birşey döndürmez.
+        IDataResult<List<Product>> GetAll(); //Tümünü getir.
+        IDataResult<List<Product>> GetAllByCategoryId(int id); //by:ile categoryId göre getir.
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId); //Tek başına bir ürün döndürür.
+        IResult Add(Product product);  
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -25,39 +26,42 @@ namespace Business.Concrete
             //Business kodlar buraya yazılır.
             if (product.ProductName.Length<2)
             {
-                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır!");
+                //Magic strings
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
             
             _productDal.Add(product);
-            return new SuccessResult("Ürün eklendi!");
+            return new SuccessResult(Messages.ProductAdded);
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
-            //İş kodları varsa...
-            //Yetkisi var mı?
-
-            return _productDal.GetAll();
+            
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p => p.CategoryId == id); //Her p için olurda p.CategoryId'si benim gönderdiğim CategoryId eşitse onları filtrele demek.
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id)); //Her p için olurda p.CategoryId'si benim gönderdiğim CategoryId eşitse onları filtrele demek.
         }
 
-        public Product GetById(int productId)
+        public IDataResult<Product> GetById(int productId)
         {
-            return _productDal.Get(p => p.ProductId == productId);
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            if (DateTime.Now.Hour == 14)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
